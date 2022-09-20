58ae237c214154362c2dd08c5ad5865a4ff94f62
Sean Gallagher <sean@gallaghersw.dev>
2022-09-20 14:52:25 -0400

    restore shorter tapping term

--- keyboards/moonlander/keymaps/seangllghr/config.h
+++ keyboards/moonlander/keymaps/seangllghr/config.h
@@ -3,8 +3,8 @@
   See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
 */
 #define ORYX_CONFIGURATOR
-/* #undef TAPPING_TERM */
-/* #define TAPPING_TERM 160 */
+#undef TAPPING_TERM
+#define TAPPING_TERM 160
 
 #define USB_SUSPEND_WAKEUP_DELAY 0
 #define RGB_MATRIX_STARTUP_SPD 60
